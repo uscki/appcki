ba7d03bb87bf105869b2a07bbedf9c38f32df78b
Tim Selier <t.selier@flexyz.com>
2015-02-27 20:54:09 +0100

    home.html

---
+++ www/angular/Home/HomeController.js
@@ -0,0 +1,7 @@
+angular.module('appcki')
+.controller("homePageCtrl", ['$scope', '$log', '$http','$location','$mdDialog','UserService',
+function($scope, $log, $http, $location, $mdDialog, UserService){	
+
+
+
+}]);
