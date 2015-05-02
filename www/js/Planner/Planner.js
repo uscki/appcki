7bd05dfc94659c796be46380688922e32c831f5b
Jan de Mooij <A.J.deMooij@gmail.com>
2015-05-02 21:17:54 +0200

    You won't believe this conceptual design!
    Een nieuwe view, tussen news en settings, voor vergaderplanner toegevoegd.
    Deze geeft de openstaande vergaderingen weer.
    Hierop klikken levert een detailweergave van een vergadering waarin dezelfde
    informatie als in de overview zichtbaar is, plus een agenda, als deze beschikbaar is, en waar gebruikers voorkeuren voor tijdssloten aan kunnen geven.
    Door een tijdsslot lang ingedrukt te houden verschijnt een detailweergave van het tijdsslot, met personen die gereageerd hebben, inclusief opmerkingen, en een mogelijkheid zelf een opmerking toe te voegen of aan te passen.
    
    De functie draait nog niet op een API en werkt met volledig hardgecodeerde content. Suggesties voor een API zijn wel toegevoegd aan de documentatie en een subfolder van de planner.

--- www/js/Planner/Planner.js
+++ www/js/Planner/Planner.js
@@ -7,6 +7,10 @@ angular
 
 			var state = PlannerService.createState();
 
+			/**
+			 * Vraagt de meetings op bij de planner service en verandert elementen
+			 * die null kunnen zijn in een passende text
+			 */
 			PlannerService.getMeetings(state, function(meetings){
 
 				for(var i = 0; i < meetings.length; i++){
@@ -19,6 +23,11 @@ angular
 				}
 			});
 
+			/**
+			 * Telt hoe veel mensen gereageerd hebben door in de tabel het aantal people te tellen waarin 
+			 *	meeting_id gelijk is aan het id van de meeting en vervolgens in meeting_preferences te kijken
+			 *	hoeveel van die mensen al daar in bestaan voor die vergadering
+			 */
 			function getNumberOfResponses(id)
 			{
 				return "3/5";
@@ -27,12 +36,18 @@ angular
 
 
 	}])
-	.controller("appckiPlannerDetails", ['$scope', '$log', '$http','$state','$stateParams','$filter','PlannerService', 'UserService',
-		function( $scope, $log, $http, $state, $stateParams, $filter, PlannerService, UserService){
+	.controller("appckiPlannerDetails", ['$scope', '$ionicModal', '$log', '$http','$state','$stateParams','$filter','PlannerService', 'UserService',
+		function( $scope, $ionicModal, $log, $http, $state, $stateParams, $filter, PlannerService, UserService){
 			
 			$scope.items = [];
 			var lastDate = 0;
 
+			/**
+			 * Get details vraagt gewoon alle timeslots op.
+			 * Vervolgens moet eigenlijk voor al die timeslots ook
+			 * worden opgevraagd wie er al gereageerd hebben, of mensen
+			 * comments hebben toegevoegd en al dat soort dingen
+			 */
 			PlannerService.getDetails(1, function(planner){
 				for(var i = 0; i < planner.length; i++)
 				{
@@ -49,33 +64,34 @@ angular
 				}
 			});
 
+			$scope.openModal = function(id)
+			{
+				console.log("show modal for id: " + id);
 
+				// Vanuit API starttijd van dit tijdsframe ophalen en vanuit
+				// de kalender berekenen wat de eindtijd is (op basis van duur van een frame)
+				$scope.modal.starttime = $scope.items[id].starttime;
+				// Vanuit de API gegevens over dit frame ophalen:
+				// - Alle namen die kunnen + comments
+				// - Alle namen die niet kunnen + comments
+				$scope.modal.kunnenwel = ['Pim hopmans', 'pim is slim'];
+				$scope.modal.kunnenniet = ['Minder slimme Pim','Echt jongens','AppCKI is reuze belangrijk'];
 
-	}])
-	.controller("appckiPlannerModal", ['$scope', 'plannerModal', '$http','$state','$filter','PlannerService', 'UserService',
-		function($scope, $plannerModal, $log, $http, $state, $filter, PlannerService, UserService){
-			$plannerModal.fromTemplateUrl('planner-modal.html', {
-				scope: $scope,
-				animation: 'slide-in-up'
-			}).then(function(modal) {
-				$scope.modal = modal;
-			});
-			$scope.openModal = function() {
+				// Vanuit de API eigen comment ophalen en mogelijk maken
+				// comment toe te voegen
+
 				$scope.modal.show();
-			};
-			$scope.closeModal = function() {
-				$scope.modal.hide();
-			};
-			//Cleanup the modal when we're done with it!
-				$scope.$on('$destroy', function() {
-				$scope.modal.remove();
-			});
-			// Execute action on hide modal
-			$scope.$on('modal.hidden', function() {
-				// Execute action
-			});
-			// Execute action on remove modal
-			$scope.$on('modal.removed', function() {
-				// Execute action
-			});
+			}
+
+  
+		    // Load the modal from the given template URL
+		    $ionicModal.fromTemplateUrl('modal.html', function($ionicModal) {
+		        $scope.modal = $ionicModal;
+		    }, {
+
+		        // Use our scope for the scope of the modal to keep it simple
+		        scope: $scope,
+		        // The animation we want to use for the modal entrance
+		        animation: 'slide-in-up'
+		    });
 	}]);
