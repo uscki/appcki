fce3a733ca361133a84e1db9ded7080b3b5e1345
Tim Selier <timselier@MacBook-Pro-van-tim.local>
2015-05-18 21:24:37 +0200

    roephoek service sluit aan op API

--- www/js/Roephoek/RoephoekService.js
+++ www/js/Roephoek/RoephoekService.js
@@ -12,17 +12,43 @@ angular
          */
         RoephoekService.getOverview = function(state, page, callback)
         {
-        	$http.get(apiUrl + "roephoek/overview&page=" + page).
+
+            $http({
+                url: "shoutbox/overview", 
+                method: "GET",
+                params: {page: page, sort: "id,desc"}
+            })
         	success(function(data){
         		callback(data);
         	});
+        }
+
+
+        RoephoekService.getNewer = function(state, page, callback)
+        {
+        /*
+            state.id = last id
+        */
+            $http({
+                url: "shoutbox/newer", 
+                method: "GET",
+                params: {id: state.id, page: page, sort: "id,desc"}
+            })
+            success(function(data){
+                callback(data);
+            });
         }
 
         RoephoekService.post = function(name, message, callback)
         {
             name = escape(name);
             message = escape(message);
-            $http.get(apiUrl + "roephoek/post?name=" + name + "&message=" + message)
+//            $http.get(apiUrl + "shoutbox/post?name=" + name + "&message=" + message)
+            $http({
+                url: "shoutbox/shout", 
+                method: "GET",
+                params: {nickname: name, message:message}
+            })
             .success(function(data){
                 callback(data);
             })
