84a760128415b2235f2f503da03c0c63792a81c7
JeromeMutgeert <jerome.mutgeert@gmail.com>
2015-05-24 00:48:17 +0200

    correctie roephoek.js

--- www/js/Roephoek/Roephoek.js
+++ www/js/Roephoek/Roephoek.js
@@ -1,46 +1,43 @@
 angular
 	.module('appcki.roephoek',[])
-	.controller("appckiRoephoekOverview", ['$scope', '$ionicPopup', '$ionicScrollDelegate', '$interval', 'RoephoekService', 'UserService', 'DateHelper',
-		function( $scope, $ionicPopup, $ionicScrollDelegate, $interval, RoephoekService, UserService, DateHelper){
+	.controller("appckiRoephoekOverview", ['$scope', '$ionicPopup', '$ionicScrollDelegate', 'RoephoekService', 'DateHelper',
+		function( $scope, $ionicPopup, $ionicScrollDelegate, RoephoekService, DateHelper){
+			var page = 0;
 			$scope.last = false;
 			var newestId;
 			var oldestId = -1;
-			
 
 			$scope.items = [];
 
-			var updateTime = function()
-			{
-				for(var i = 0; i < $scope.items.length; i++)
-				{
-					var oldwhen = $scope.items[i].when;
-					$scope.items[i].when = DateHelper.difference($scope.items[i].timestamp);
-					if(oldwhen == $scope.items[i].when)
-					{
-						break;
-					}
+			/*RoephoekService.getOverview(page, function(data){
+				console.log(data);
+				$scope.last = data.last;
+				newestId = data.content[0].id;
+				for(var i = 0; i < data.content.length; i++) {
+					var item = data.content[i];
+					item.when = DateHelper.difference(item.timestamp);
+					$scope.items.push(item);
 				}
-			}
+			}), function(){};*/
 
 			$scope.doRefresh = function(){
 				var items = [];
 				RoephoekService.getNewer(newestId, function(data){
-					newestId = (data.content.length > 0) ? data.content[0].id : newestId;
 					for(var i = data.content.length; i > 0; i--)
 					{
 						var item = data.content[i-1];
 						item.when = DateHelper.difference(item.timestamp);
 						$scope.items.unshift(item);
 					}
-					updateTime();
-				},
-				function(){
+
+				}, function(){
 					$scope.$broadcast('scroll.refreshComplete');
 				});
 			}
 
 			$scope.loadMoreData = function()
 			{
+				console.log("Laden...");
 				RoephoekService.getOlder(oldestId, function(data){
 					$scope.last = data.last;
 					
@@ -50,7 +47,7 @@ angular
 					for(var i = 0; i < data.content.length; i++) {
 						var item = data.content[i];
 						item.when = DateHelper.difference(item.timestamp);
-						$scope.items.unshift(item);
+						$scope.items.push(item);
 					}
 				}, function(){
 					$scope.$broadcast('scroll.infiniteScrollComplete');
@@ -59,11 +56,10 @@ angular
 			}
 
 			$scope.openShout = function(){
-				$scope.shout = {};
-				$scope.shout.name = UserService.fullname();
+				console.log("ay");
 			  $ionicPopup.show({
-			     template: '<input type="text" ng-model="shout.name" placeholder="Naam" maxlength="26"/><textarea ng-model="shout.message" maxlength="161" style="height:80px;" placeholder="Bericht"> </textarea>',
-			     title: 'WatRoepJeMeNou',
+			     template: '<textarea ng-model="data.note" style="height:80px;"> </textarea>',
+			     title: 'Roep',
 			     subTitle: '',
 			     scope: $scope,
 			     buttons: [
@@ -72,27 +68,14 @@ angular
 			         text: '<b>Roep!</b>',
 			         type: 'button-positive',
 			         onTap: function(e) {
-			             return $scope.shout;
+			             return $scope.data.note;
 			         }
 			       },
 			     ]
 			   }).then(function(res) {
-					RoephoekService.post($scope.shout.name, $scope.shout.message, function(d){
-						$scope.doRefresh();
-					}, function(d){
-						var alertPopup = $ionicPopup.alert({
-							title: 'Mislukt',
-							template: 'Je roep kon niet worden verstuurd. Controleer of je alles hebt ingevuld en ga zitten balen, tot het we werkt!'
-						});
-					});
+			   	console.log($scope.data.note);
+			   	console.log(res);
+			   	RoephoekService.shout(agenda.id, res, function(result){});
 			   });			
 			};
-
-			$interval(function(){
-				if(newestId)
-				{
-					$scope.doRefresh();
-				}
-			}, 30000);
-			
 	}]);
